5cb7142bf4ae10da3d6a40a1b22b76d2d365c1dc
gykcode <mendoza.iggyfrancis@gmail.com>
2023-03-09 11:24:42 +0800

    commit

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteTask, UserLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
@@ -13,4 +18,9 @@ urlpatterns = [
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-remove/<int:pk>/', DeleteTask.as_view(), name='task-remove'),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
+
--- todo_app/settings.py
+++ todo_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sanwcp3#ghr_66#2v_92*n+mi0m*k1h&3%bh9)osmv^00rvk^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
